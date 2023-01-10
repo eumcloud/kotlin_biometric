770f7eb4d88495c49ee6787e4e8dd9d65ac3043f
eumcloud <livelch012@naver.com>
2023-01-10 15:50:10 +0900

    add: camera func - ing..

--- app/src/main/java/com/metehanbolat/biometricauthentication/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/biometricauthentication/MainActivity.kt
@@ -1,32 +1,60 @@
 package com.metehanbolat.biometricauthentication
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.icu.text.SimpleDateFormat
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.biometric.BiometricManager
 import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
 import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
 import androidx.biometric.BiometricPrompt
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.metehanbolat.biometricauthentication.databinding.ActivityMainBinding
+import java.io.File
+import java.util.*
 import java.util.concurrent.Executor
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+
+private val ImageView.surfaceProvider: Preview.SurfaceProvider?
+    get() = null
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
     val TAG = "MainActivity"
+//    BIOMETRIC val
     private lateinit var executor: Executor
     private lateinit var biometricPrompt: BiometricPrompt
     private lateinit var promptInfo: BiometricPrompt.PromptInfo
+
+//    CAMERA val
+    private val imageCapture:ImageCapture?=null
+    private lateinit var outputDirectory:File
+    private lateinit var cameraExecutor:ExecutorService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+//        camera
+        outputDirectory = getOutputDirectory()
+        cameraExecutor = Executors.newSingleThreadExecutor()
 
         /** In this section, you can take action according to the status and information of the biometric hardware of the device. */
         val biometricManager = BiometricManager.from(this)
@@ -47,6 +75,17 @@ class MainActivity : AppCompatActivity() {
             .build()
 
         executor = ContextCompat.getMainExecutor(this)
+
+
+        if(allPermissionGranted()){
+            startCamera()
+        }else{
+            ActivityCompat.requestPermissions(
+                this, Constants.REQUIRED_PERMISSIONS,
+                Constants.REQUEST_CODE_PERMISSIONS
+            )
+        }
+
 
         biometricPrompt = BiometricPrompt(this, executor,
             object : BiometricPrompt.AuthenticationCallback() {
@@ -71,7 +110,104 @@ class MainActivity : AppCompatActivity() {
 
         binding.button.setOnClickListener {
             biometricPrompt.authenticate(promptInfo)
+            takePhoto()
         }
 
     }
+    private fun getOutputDirectory():File{
+        val mediaDir = externalMediaDirs.firstOrNull()?.let{
+            mFile->
+                File(mFile, resources.getString(R.string.app_name)).apply{
+                    mkdirs()
+                }
+        }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
+    }
+    private fun takePhoto() {
+        val imageCapture = imageCapture ?: return
+        val photofile = File(
+            outputDirectory,
+            SimpleDateFormat(Constants.FILE_NAME_FORMAT,
+                Locale.getDefault())
+                    .format(System.currentTimeMillis())+".jpg")
+        val outputOption = ImageCapture
+            .OutputFileOptions
+            .Builder(photofile)
+            .build()
+
+        imageCapture.takePicture(
+            outputOption, ContextCompat.getMainExecutor(this),
+            object :ImageCapture.OnImageSavedCallback{
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    val savedUri = Uri.fromFile(photofile)
+                    val msg = "Photo Saved"
+
+                    Toast.makeText(this@MainActivity, "$msg $savedUri", Toast.LENGTH_LONG).show()
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Log.e(TAG, "onError:${exception.message}", exception)
+                }
+
+            }
+        )
+
+    }
+
+    private fun allPermissionGranted() =
+        Constants.REQUIRED_PERMISSIONS.all{
+            ContextCompat.checkSelfPermission(
+                baseContext, it
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array< String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == Constants.REQUEST_CODE_PERMISSIONS){
+            if(allPermissionGranted()){
+                startCamera()
+            }else{
+                Toast.makeText(this,
+                "Permissions not granted",
+                Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    override fun onDestroy(){
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance( this )
+        cameraProviderFuture.addListener({
+            val cameraProvider : ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder()
+              .build()
+              .also{ mPreview->
+                  mPreview.setSurfaceProvider(
+                      binding.viewFinder.surfaceProvider
+                  )
+            }
+
+//        imageCapture = ImageCapture.Builder().build()
+        val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            try{
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this,
+                    cameraSelector,
+                    preview,
+                    imageCapture
+                )
+            }catch(e: Exception){
+                Log.d(TAG, "start Camera Failed", e)
+            }
+        }, ContextCompat.getMainExecutor(this))
+
+    }
+
 }
